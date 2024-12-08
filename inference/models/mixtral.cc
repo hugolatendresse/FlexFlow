12176ce0cb078584e6fbb135f85004b345472413
Ubuntu <ubuntu@ip-172-31-39-48.ec2.internal>
2024-12-08 16:45:36 +0000

    no softmax

--- inference/models/mixtral.cc
+++ inference/models/mixtral.cc
@@ -282,30 +282,11 @@ void MIXTRAL::create_mixtral_model(FFModel &ff,
     Tensor topk_values_reduced = ff.reduce_sum(topk_values, {0}, true); // (2, 1, 1)
     topk_values = ff.divide(topk_values, topk_values_reduced); // (2, 1, 128)
 
-    Tensor dummy_gate = ff.dense(
-        ff_norm,
-        mixtral_config.num_local_experts,
-        AC_MODE_NONE,
-        false,
-        DT_NONE,
-        nullptr,
-        nullptr,
-        nullptr,
-        REG_MODE_NONE,
-        0.0f,
-        std::string("layers." + std::to_string(i) + ".block_sparse_moe_gate")
-            .c_str());
-
-    dummy_gate = ff.softmax(
-        gate,
-        0,
-        DT_NONE,
-        std::string("dummy_gate").c_str());
-
+  
     aggregate_inputs[0] = topk_values; // (experts_per_tok, 1, 128) (3 dims confirmed)
     aggregate_inputs[1] = topk_indices; // (experts_per_tok, 1, 128) (3 dims confirmed)
     aggregate_inputs[2] = topk_values; // TODO this is a tmp fix
-    aggregate_inputs[3] = dummy_gate;  // TODO this is a tmp fix
+    aggregate_inputs[3] = gate;  // TODO this is a tmp fix
 //    aggregate_inputs[2] = aggregate_inputs[3] = nullptr;
     mlp_out = ff.aggregate(aggregate_inputs,
                            mixtral_config.num_local_experts,
