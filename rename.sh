1c13c1e0ad7fb7b49545e12858b8517fe5e2e532
Hugo Latendresse <hugolatendresse@gmail.com>
2024-12-08 22:08:12 -0500

    sync

--- rename.sh
+++ rename.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Target directory
-TARGET_DIR="/root/.cache/flexflow/weights/m4-ai/tinymistral-6x248m/half-precision"
+TARGET_DIR="/root/.cache/flexflow/weights/m4-ai/tinymistral-6x248m/full-precision"
 
 # Loop through files containing "layer_"
 for file in "$TARGET_DIR"/*layer_*; do
