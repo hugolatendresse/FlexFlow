303cc8a18de60541f8aeb74f11e8f9d61e846e99
Ubuntu <ubuntu@ip-172-31-39-48.ec2.internal>
2024-12-09 05:03:11 +0000

    print topk values

--- inference/models/mixtral.cc
+++ inference/models/mixtral.cc
@@ -208,8 +208,8 @@ void MIXTRAL::create_mixtral_model(FFModel &ff,
         std::string("layers." + std::to_string(i) + ".block_sparse_moe_topk")
             .c_str());
     Tensor topk_values = topk_out[0]; // (experts_per_tok, 1, 128) (confirmed 3 dims)
-    cout << topk_values->num_dims;
     Tensor topk_indices = topk_out[1]; // (experts_per_tok, 1, 128) (confirmed 3 dims)
+    printf("TOPK VALUES DIM %d", topk_values->num_dims);
 
     Tensor grouped_tokens[mixtral_config.num_local_experts] = {nullptr};
     ff.group_by(
