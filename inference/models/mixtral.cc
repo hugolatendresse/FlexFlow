c3259c8c4410588b4653d49f4998162f5d5ff13b
Ubuntu <ubuntu@ip-172-31-39-48.ec2.internal>
2024-12-10 22:16:38 +0000

    don't use reduce_sum or divide. Reduce sum has no init_inference. Divide has no materialize (it is binary op. but add binary op does?). BROKEN

--- inference/models/mixtral.cc
+++ inference/models/mixtral.cc
@@ -291,7 +291,7 @@ void MIXTRAL::create_mixtral_model(FFModel &ff,
     //                                    ".divide_topk")
     //                            .c_str()); // (2, 1, 128)
 
-    Tensor output = aggregate_inputs;
+    Tensor output = aggregate_inputs[0];
 
 //     aggregate_inputs[0] = topk_values; // (experts_per_tok, 1, 128) (3 dims confirmed)
 //     aggregate_inputs[1] = topk_indices; // (experts_per_tok, 1, 128) (3 dims confirmed)
