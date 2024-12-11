628d2f278b0ffb5fb1f9f6424221e1c173ee52cb
Ubuntu <ubuntu@ip-172-31-39-48.ec2.internal>
2024-12-11 04:41:12 +0000

    more groupby print

--- src/ops/group_by.cc
+++ src/ops/group_by.cc
@@ -78,7 +78,7 @@ void FFModel::group_by(const Tensor input,
     } else { // MK: added this for dummy groupby
       dims[num_dims - 1] = 128;
     }
-    printf("ff.groupby output: %d", num_dims);
+    printf("ff.groupby output: %d %d %d %d\n", dims[0], dims[1], dims[2], dims[3]);
 
     for (int i = 0; i < num_local_experts; i++) {
       // Creating one tensor per expert, each with size (DATA_DIMS,
@@ -167,15 +167,16 @@ Group_by::Group_by(FFModel &model,
 
   // dims[num_dims - 2].size = (int)ceil(alpha * k_experts_per_tok / n * inputs[0]->dims[1].size);
   // MK why is this - 2 instead of - 1? Also, why no alpha?
-  printf("groupby op output: %d %d %d %d %d\n", dims[0], dims[1], dims[2], dims[3], dims[4]);
+  
   if (alpha != 0.0f) {
 //      int seq_len = input->dims[num_dims - 1];
-      dims[num_dims - 1].size = (int)ceil(alpha * k_experts_per_tok / n * inputs[0]->dims[1].size);
+      dims[num_dims - 2].size = (int)ceil(alpha * k_experts_per_tok / n * inputs[0]->dims[1].size);
     
   } else { // MK: added this for dummy groupby
-    dims[num_dims - 1].size = 128;
+    dims[num_dims - 2].size = 128;
     //dims[num_dims - 2] = 128;
   }
+  printf("groupby op output: %d %d %d %d\n", dims[0], dims[1], dims[2], dims[3]);
   // dims[num_dims - 2].size = 128;
 //  printf("max num tokens dim in output used to be %d\n", (int)ceil(alpha * k_experts_per_tok / n * inputs[0]->dims[1].size));
 
