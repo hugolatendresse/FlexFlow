eea27207551421f94062bdea680d8cb39e7a5b31
Hugo Latendresse <hugolatendresse@gmail.com>
2024-12-11 13:05:41 -0500

    sync

--- inference/models/mixtral.cc
+++ inference/models/mixtral.cc
@@ -376,7 +376,7 @@ void MIXTRAL::create_mixtral_model(FFModel &ff,
     aggregate_inputs[2] = topk_values_DUMMY;
     aggregate_inputs[3] = gate_DUMMY;
 //
-    Tensor mlp_out = ff.aggregate(aggregate_inputs,
+    mlp_out = ff.aggregate(aggregate_inputs,
                            mixtral_config.num_local_experts,
                            0.0f,
                            std::string("layers." + std::to_string(i) +
